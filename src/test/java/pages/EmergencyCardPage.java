39bd84ad4531d08490104be7cce745d0e73971d6
ClocrTeam <new.clocr@gmail.com>
2022-02-01 17:28:33 +0530

    Commit from branch

--- src/test/java/pages/EmergencyCardPage.java
+++ src/test/java/pages/EmergencyCardPage.java
@@ -6,13 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class EmergencyCardPage extends BasePage {
+import org.openqa.selenium.support.FindBy;
+
+public class EmergencyCardPage extends BasePage 
+{
 
 	private WebDriver driver;
 	public EmergencyCardPage(WebDriver driver) {
